2e13025baa64c0d0ab51b15f0873f0e93c401b9e
thomacer <thomas.perale@openmailbox.org>
2015-08-24 23:21:23 +0200

    install script

---
+++ fresh_install.sh
@@ -0,0 +1,25 @@
+m="pacman -S"
+
+$m git
+$m npm
+$m nodejs
+$m python2
+$m python3
+$m gcc-multilib
+$m vim-python3
+$m firefox
+$m newsbeuter-git
+$m ranger
+$m mpv
+$m irssi
+$m zathura
+$m dunst
+$m compton
+$m python-pip
+$m python2-pip
+$m fish
+chsh -s /usr/bin/fish
+
+
+npm install peerflix
+pip install youtube-dl
