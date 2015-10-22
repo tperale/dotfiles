ced3d136afd794013965421de673c253da84562f
Perale Thomas <thomas.perale@openmailbox.org>
2015-10-22 20:45:27 +0200

    functions fish

--- .config/fish/functions/function.fish
+++ .config/fish/functions/function.fish
@@ -1,7 +1,14 @@
-function memo
-    echo "$argv" | openmail -b "thomas.perale@openmailbox.org"
+# FASD function for fish shell (https://github.com/clvv/fasd) {{{
+function -e fish_preexec _run_fasd
+  fasd --proc (fasd --sanitize "$argv") > "/dev/null" 2>&1
 end
 
+function j
+    cd (fasd -d -e 'printf %s' "$argv")
+end
+# }}}
+
+# FUCK (https://github.com/nvbn/thefuck) {{{
 function fuck -d 'Correct your previous console command'
     set -l exit_code $status
     set -l eval_script (mktemp 2>/dev/null ; or mktemp -t 'thefuck')
@@ -13,12 +20,18 @@ function fuck -d 'Correct your previous console command'
         history --delete $fucked_up_command
     end
 end
+# }}}
+
+# {{{
+function memo
+    echo "$argv" | openmail -b "thomas.perale@openmailbox.org"
+end
 
 function record
     ffmpeg -f x11grab -r 25 -s wxga -i :0.0 /tmp/$argv.mp4
 end
 
-
 function yt2mp3
     ytdl -c --restrict-filenames --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" $argv;
 end
+# }}}
