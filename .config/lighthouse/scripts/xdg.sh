05b84c8bba1b1bdeb744b5e7bd4e9f069876ce29
Perale Thomas <thomas.perale@openmailbox.org>
2015-12-30 19:58:06 +0100

    Update application finder

--- .config/lighthouse/scripts/xdg.sh
+++ .config/lighthouse/scripts/xdg.sh
@@ -1,20 +1,35 @@
 #!/bin/env bash
 
 function search_xdg() {
+    RES=""
     for app in $(ls $LOCATION | grep $1)
     do
+
+        NAME=$(echo $app | awk -F. '{ print $1 }')
+
         # Get name and desc, etc
-        NAME=$(cat $LOCATION$app | grep "Name=" | sed -n '1p' | sed 's/Name=//') # NAME
-        COMMENT=$(cat $LOCATION$app | grep "Comment=" | sed -n '1p' | sed 's/Comment=//') # DESCRIPTION
+        COMMENT=$(cat $LOCATION$app | grep "GenericName=" | awk -F= '{ print $2 }')
+        DESC=$(cat $LOCATION$app | grep "Comment=" | awk -F= '{ print $2 }')
+
+        EXEC=""
         $(cat $LOCATION$app | grep "Terminal=true" | sed -n '1p' | sed 's/Terminal=//') # EXEC
         if (($?)); then # If it find true it mean the app should be exec in a terminal.
             EXEC=$TERMINAL
         fi
-        EXEC=$EXEC$(cat $LOCATION$app | grep "Exec=" | sed -n '1p' | sed 's/Exec=//')
+        EXEC=${EXEC}$(cat $LOCATION$app | grep "Exec=" | awk -F= '{ print $2 }' | awk '{ print $1 }')
+
+
+        RES=${RES}"{${NAME}|${EXEC}|"
         if [ ${#COMMENT} ]; then
-            printf "{$NAME|$EXEC|%%C$COMMENT %%}\n"
+            RES="${RES}%C${COMMENT}%%L"
         fi
+        if [ ${#DESC} ]; then
+            RES="${RES}%C${DESC}%"
+        fi
+        RES=${RES}"}"
     done
+
+    echo "${RES}"
 }
 
 EXEC=""
