336e0fb5344ab858c1d261acd9c51b1e7e0edb37
Perale Thomas <thomas.perale@openmailbox.org>
2017-01-03 01:10:43 +0100

    Fix multiple editors error

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -3,6 +3,6 @@ set PATH $PATH /home/thomas/pebble-dev/pebble-sdk-last/bin
 set ANDROID_HOME $ANDROID_HOME ~/Android/Sdk
 set PATH $PATH ~/Android/Sdk/tools 
 set PATH $PATH ~/Android/Sdk/platform-tools 
-set BROWSER $BROWSER firefox
-set EDITOR $EDITOR vim
-set VISUAL $VISUAL vim
+set BROWSER firefox
+set EDITOR vim
+set VISUAL vim
