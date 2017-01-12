3bd3b3056671f2ff4bc94033a9465f6587560ef4
Perale Thomas <thomas.perale@openmailbox.org>
2017-01-12 13:28:58 +0100

    Update .env

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,8 +1,10 @@
 set PATH $PATH /home/thomas/bin
 set PATH $PATH /home/thomas/pebble-dev/pebble-sdk-last/bin
-set ANDROID_HOME $ANDROID_HOME ~/Android/Sdk
 set PATH $PATH ~/Android/Sdk/tools 
 set PATH $PATH ~/Android/Sdk/platform-tools 
-set BROWSER firefox
-set EDITOR vim
-set VISUAL vim
+set ANDROID_HOME ~/Android/Sdk
+set -g XDG_CONFIG_HOME ~/.config/
+set -g XDG_CONFIG ~/.config/
+set -g BROWSER firefox
+set -g EDITOR vim
+set -g VISUAL vim
