48f981ea712b6b45c3d640b22445c67fe3b5445b
Perale Thomas <thomas.perale@openmailbox.org>
2017-01-15 21:05:11 +0100

    Fix .env var

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,10 +1,16 @@
+set -x LANG "en_US.UTF-8"
+set -x LC_ALL "en_US.UTF-8"
+set -x XDG_CONFIG_HOME ~/.config/
+set -x XDG_CONFIG ~/.config/
+set -x BROWSER firefox
+set -x EDITOR vim
+set -x VISUAL vim
+
+set -g EDITOR vim
+set -g VISUAL vim
+
 set PATH $PATH /home/thomas/bin
 set PATH $PATH /home/thomas/pebble-dev/pebble-sdk-last/bin
 set PATH $PATH ~/Android/Sdk/tools 
 set PATH $PATH ~/Android/Sdk/platform-tools 
 set ANDROID_HOME ~/Android/Sdk
-set -g XDG_CONFIG_HOME ~/.config/
-set -g XDG_CONFIG ~/.config/
-set -g BROWSER firefox
-set -g EDITOR vim
-set -g VISUAL vim
